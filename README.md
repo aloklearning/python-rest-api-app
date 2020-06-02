7d0d63bc07ae2503c3ed26534fa4c0a68b1bf2b9
Alok <alokkumar@pinelane.in>
2020-06-02 23:13:36 +0530

    Initial Project Created

---
+++ README.md
@@ -0,0 +1,4 @@
+# PYTHON REST API USING FLASK
+
+This project is a learning project created on the basis of learning from the Udemy course **REST APIs with FLASK and PYTHON**
+
---
+++ app.py
@@ -0,0 +1,7 @@
+# This is a python app file which will help connecting with the flask
+from flask import Flask
+
+# Creating an app from the Flask class
+app = Flask(__name__)
+
+
