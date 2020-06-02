29c4dcb1863f2eb46b636a21e5531420cee87475
Alok <alokkumar@pinelane.in>
2020-06-03 00:02:21 +0530

    Ran the first Flask app returing response

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This project is a learning project created on the basis of learning from the Udemy course **REST APIs with FLASK and PYTHON**
 
+## Prereuisites
+
+
+## Getting Started
+
+- Make sure you are inside the project directory
+- The python project runs on flask, using the command `python<version> app.py`
--- app.py
+++ app.py
@@ -1,7 +1,29 @@
-# This is a python app file which will help connecting with the flask
+'''
+This is a python app file which will help connecting with the flask
+flask is a file name, starts with small f, having a class Flask,
+starts with a capital one
+'''
 from flask import Flask
 
-# Creating an app from the Flask class
+# Creating an object app from the Flask class with a unique name(__name__)
 app = Flask(__name__)
+
+'''
+Now we have made the application to be able to take the request
+We are now going to make the project listen to a particular
+request and give out a response to the request maker
+Request forward slash, like https://google.com/, so this is a route
+
+Mapping User[Request with route] -> Browser -> Server[FLASK] -> Data Response which we will do here
+'''
+@app.route('/') # Python decorater route from Flask class followed by method
+def home():
+    # This response is the one which will be shown by the browser to the 
+    # the request maker
+    return 'Hello World!'
+
+# This tells the app to run the app on a local server provided the port 
+# in the run()
+app.run(port=5000)
 
 
