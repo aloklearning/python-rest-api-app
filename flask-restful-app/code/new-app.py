762f36ba1dac2a9bb640a25ec0fb44d8d691c18c
Alok <alokkumar@pinelane.in>
2020-06-05 16:09:31 +0530

    Update and delete operation done with parser

--- flask-restful-app/code/new-app.py
+++ flask-restful-app/code/new-app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 #ignore import error cos we this is not defined in our global Python Library list
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required #JSON Web Token(JWT), allows us to decode, verify and generate JWT
 
 from security import identity, authenticate
@@ -26,6 +26,12 @@ items = []
 
 # Every resources has to be a class, which will inherit the Resource
 class Item(Resource):
+    # reqparser to be used for parsing the form data, also
+    # if some other data was passed in the JSON form data
+    # it ignores and takes only the specified param from the request
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, required=True, help='Price cannot be left empty!')
+    
     # this get method is from resource, which represents the GET method
     # jwt will autheticate and then get the data here
     @jwt_required()
@@ -51,10 +57,13 @@ class Item(Resource):
             # 400 Status code for Bad Request
             return {'message': "The item is already there with the name '{ }'.".format(name)}, 400
         
-        # for JSON payload data get request.get_json()
-        # silent=True or force=True are the params use to ignore the 
-        # the Header Content-Type, if it is not being passed by the user 
-        data = request.get_json() 
+        '''
+        for JSON payload data get request.get_json()
+        silent=True or force=True are the params use to ignore the 
+        the Header Content-Type, if it is not being passed by the user 
+        '''
+        data = Item.parser.parse_args() # data = {"price": 15.99}, data['price'] = 15.99
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201 #flask)restful way of sending status 201 which is for CREATED STATUS
@@ -66,6 +75,18 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'The item has been successfully deleted'}
 
+    # This is for HTTP PUT i.e., CREATE/UPDATE
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # if item is not found, then create a new item
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data) # updates the item with the data received, if found
+        return item
 
 class ItemList(Resource):
     def get(self):
