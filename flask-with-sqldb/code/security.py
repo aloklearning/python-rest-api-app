23d98831ad17419edc451dba987a546246cd1a00
Alok <alokkumar@pinelane.in>
2020-06-09 12:22:09 +0530

    User authentication using sqlite done

--- flask-with-sqldb/code/security.py
+++ flask-with-sqldb/code/security.py
@@ -2,19 +2,20 @@
 # from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1, 'Bob', 'asdf')
-]
+# users = [
+#     User(1, 'Bob', 'asdf')
+# ]
 
 # Retrieve the user by username
 # Retrieve the user by user_id
-username_mapping = {u.username: u for u in users} # {'Bob': User list object}
-userid_mapping = {u.id: u for u in users} # {1: User list object}
+# username_mapping = {u.username: u for u in users} # {'Bob': User list object}
+# userid_mapping = {u.id: u for u in users} # {1: User list object}
 
 # This is for authentication purpose
 #Generates JWT Token after performing this action
 def authenticate(username, password):
-    user = username_mapping.get(username, None) # <user.User object at 0x10ed0eb10>
+    #user = username_mapping.get(username, None) # <user.User object at 0x10ed0eb10>
+    user = User.find_by_username(username)
     if user and user.password == password: #safe_str_cmp(user.password, password)
         return user # Becomes identiy for the JWT Token for authentication
 
@@ -29,5 +30,6 @@ That means that was a valid JWT token passsed by the user
 '''
 def identity(payload):
     user_id = payload['identity'] # identity is the user_id
-    return userid_mapping.get(user_id, None)
+    #return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
 
